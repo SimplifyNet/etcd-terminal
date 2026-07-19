709674f1cfae279d87b966040c47fc71c59438ed
Alexanderius <alexander.krylkov@gmail.com>
2026-07-19 20:57:25 +0500

    [r] move DI registration before console setup

--- src/EtcdTerminal.App/Program.cs
+++ src/EtcdTerminal.App/Program.cs
@@ -9,21 +9,24 @@ const string ResetBgCommand = "\x1b]111\x07";
 const string ShuttingDown = "[yellow]Shutting down...[/]";
 const string PressAnyKeyRestart = "\n[grey]Press any key to restart...[/]";
 
+DIContainer.Current
+	.RegisterAll()
+	.Verify();
+
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.Write(SetBgCommand);
 
 Console.CancelKeyPress += (_, args) =>
 {
 	args.Cancel = true;
+
 	Console.Write(ResetBgCommand);
 	Console.ResetColor();
 	Console.WriteLine();
 	AnsiConsole.MarkupLine(ShuttingDown);
+
 	Environment.Exit(0);
 };
-
-DIContainer.Current.RegisterAll()
-	.Verify();
 
 while (true)
 {
@@ -38,6 +41,7 @@ while (true)
 			return;
 
 		var mainScreen = scope.Resolver.Resolve<MainScreen>();
+
 		await mainScreen.ShowAsync(config);
 	}
 	catch (Exception ex)
