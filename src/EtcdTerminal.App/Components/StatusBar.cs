8cc137b3b7a964ab866fad1ef9649ee981899010
Alexanderius <alexander.krylkov@gmail.com>
2026-07-19 21:59:02 +0500

    [edit] restyle StatusBar colors, icons, and padding calculation

--- src/EtcdTerminal.App/Components/StatusBar.cs
+++ src/EtcdTerminal.App/Components/StatusBar.cs
@@ -8,13 +8,19 @@ public static class StatusBar
 	public static void Render(EtcdConnectionConfig? config = null)
 	{
 		var bgSeq = "\x1b[48;2;27;28;30m";
-		var fgSeq = "\x1b[38;2;210;210;210m";
+		var fgSeq = "\x1b[38;2;128;128;128m";
 		var greenSeq = "\x1b[38;2;0;200;0m";
+		var whiteSeq = "\x1b[38;2;255;255;255m";
+		var tealSeq = "\x1b[38;2;0;180;180m";
+		var yellowSeq = "\x1b[38;2;255;200;0m";
+		var dimSeq = "\x1b[38;2;80;80;80m";
 		var resetSeq = "\x1b[0m";
 		var fill = new string(' ', Console.WindowWidth);
 
-		var left = "  (\u2191/\u2193 navigate, Enter confirm, Esc back)  ";
+		var left = $"{fgSeq}  {whiteSeq}\u2191/\u2193{fgSeq} navigate \u00b7 {whiteSeq}Enter{fgSeq} confirm/select \u00b7 {whiteSeq}Esc{fgSeq} back  ";
 		var version = GetVersion();
+
+		var rightPadding = "  ";
 
 		string content;
 		if (config is not null)
@@ -22,20 +28,22 @@ public static class StatusBar
 			var connStr = config.ConnectionString.Length > 50
 				? config.ConnectionString[..50] + "..."
 				: config.ConnectionString;
-			var auth = config.IsAuthenticationEnabled
-				? $" \x1b[38;2;128;128;128m│\x1b[38;2;210;210;210m {config.Username}"
-				: "";
-			var right = $"{greenSeq}\u25cf{fgSeq} {config.Name} \x1b[38;2;128;128;128m│\x1b[38;2;210;210;210m {connStr}{auth}  v{version}  ";
-			var pad = Console.WindowWidth - left.Length - GetVisibleLength(right);
-			if (pad < 1) pad = 1;
-			content = bgSeq + fgSeq + left + new string(' ', pad) + right + resetSeq;
+			var right = $"{greenSeq}\u2022{tealSeq} {config.Name} {dimSeq}\u2502{fgSeq} {connStr}";
+			if (config.IsAuthenticationEnabled)
+				right += $" {dimSeq}\u2502{yellowSeq} {config.Username}";
+			right += $" {fgSeq}v{whiteSeq}{version}";
+			var visibleWidth = GetVisibleLength(left) + GetVisibleLength(right) + rightPadding.Length;
+			var pad = Console.WindowWidth - visibleWidth;
+			if (pad < 0) pad = 0;
+			content = bgSeq + fgSeq + left + new string(' ', pad) + right + fgSeq + rightPadding + resetSeq;
 		}
 		else
 		{
-			var right = $"v{version}  ";
-			var pad = Console.WindowWidth - left.Length - right.Length;
-			if (pad < 1) pad = 1;
-			content = bgSeq + fgSeq + left + new string(' ', pad) + right + resetSeq;
+			var right = $"{fgSeq}v{whiteSeq}{version}";
+			var visibleWidth = GetVisibleLength(left) + GetVisibleLength(right) + rightPadding.Length;
+			var pad = Console.WindowWidth - visibleWidth;
+			if (pad < 0) pad = 0;
+			content = bgSeq + fgSeq + left + new string(' ', pad) + right + fgSeq + rightPadding + resetSeq;
 		}
 
 		Console.CursorTop = Console.WindowHeight - 3;
